79c79d9e6418e3f5f3788048ebd8851c005764a8
gbMichelle <gbmichelle.dev@gmail.com>
2019-04-01 10:47:07 +0200

    Added a network related global.

---
+++ network/network_globals.hpp
@@ -0,0 +1,8 @@
+Signature(true, sig_connection_type, {0x66, 0x83, 0x3D, -1, -1, -1, -1, 0x02, 0x75, -1, 0x8B});
+enum ConnectionType : uint32_t {
+    NONE = 0,
+    CLIENT = 1,
+    HOST = 2
+};
+
+ConnectionType* connection_type = reinterpret_cast<ConnectionType*>(sig_connection_type.get_address() + 3);
