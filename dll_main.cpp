35f2aa67d95e832967d6a8bf2692b898e5862dfb
gbMichelle <gbmichelle.dev@gmail.com>
2019-04-05 08:12:29 +0200

    Added incoming_packet_hooks to main as a temporary test for now.

--- dll_main.cpp
+++ dll_main.cpp
@@ -1,6 +1,6 @@
 #include "hooks/hooker.hpp"
 #include "fox.h"
-//#include "hooks/incoming_packets.hpp" // This is just for a quick test.
+#include "hooks/incoming_packets.hpp" // This is just for a quick test.
 #define WIN32_MEAN_AND_LEAN
 #include <Windows.h>
 #include "popout_console/guicon.hpp"
@@ -33,12 +33,14 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
         );
 
         // Check if the exe is a Server.
-        Signature(false, server_sig,
+        Signature(false, sig_server,
             { 0x75, 0x2D, 0x68, -1, -1, -1, -1, 0xE8, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0xE8, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x33, 0xC0, 0xE8, -1, -1, -1, -1, 0x83, 0xC4, 0x14, 0x8B, 0x44, 0x24, 0x04, 0x68, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x50 });
-        uintptr_t is_server = server_sig.get_address();
+        uintptr_t is_server = sig_server.get_address();
         if (is_server){
             printf("We're a server\n");
         };
+
+        init_incoming_packet_hooks();
     }
     else if(fdwReason == DLL_PROCESS_DETACH && loaded) {
         loaded = false;
