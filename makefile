058941567a370a23d75b87f3f4fce197cd6b538f
gbMichelle <gbmichelle.dev@gmail.com>
2019-04-05 16:08:19 +0200

    Moved initialization code out of dll_main (Finally!)

--- makefile
+++ makefile
@@ -1,18 +1,19 @@
 ARGS= -std=c++17 -masm=intel -m32
 ARGSDEF= $(ARGS) -O2
 ARGSFAST= $(ARGS) -Ofast -msse3
-LINKARGS= -m32 -Wl,--exclude-all-symbols -s
+LINKARGS= -m32 -W
+#,--exclude-all-symbols -s
 
 build:
 	mkdir -p bin
 	rm -f bin/*.o
 	rm -f bin/vulpes.dll
 	#windres version.rc -o bin/version.o
-	g++ -c dll_main.cpp $(ARGS) -o bin/dll_main.o
+	g++ -c dll_main.cpp $(ARGSDEF) -o bin/dll_main.o
+	g++ -c vulpes.cpp $(ARGSDEF) -o bin/vulpes.o
 	g++ -c popout_console/guicon.cpp -m32 -O2 -o bin/popout_console__guicon.o
 	
 	g++ -c hooks/hooker.cpp $(ARGSDEF) -o bin/hooks__hooker.o
-	
 	g++ -c hooks/incoming_packets.cpp $(ARGS) -o bin/hooks__incoming_packets.o
 	
 	g++ bin/*.o -shared $(LINKARGS) -static-libgcc -o "bin/Vulpes.dll"
---
+++ vulpes.cpp
@@ -0,0 +1,55 @@
+#define WIN32_MEAN_AND_LEAN
+#include <Windows.h>
+#include <cstdio>
+#include <cstdint>
+
+static bool server;
+
+#include "hooks/hooker.hpp"
+#include "hooks/incoming_packets.hpp"
+void init_hooks(){
+    init_incoming_packet_hooks();
+}
+void revert_hooks(){
+    revert_incoming_packet_hooks();
+}
+
+#include "fox.h"
+#include "popout_console/guicon.hpp"
+
+static SignatureBounded(true, sig_text_segment_data, 0x400000, 0x401000,
+    {0x2E, 0x74, 0x65, 0x78, 0x74, 0x00, 0x00, 0x00});
+static Signature(false, sig_server,
+    { 0x75, 0x2D, 0x68, -1, -1, -1, -1, 0xE8, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0xE8, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x33, 0xC0, 0xE8, -1, -1, -1, -1, 0x83, 0xC4, 0x14, 0x8B, 0x44, 0x24, 0x04, 0x68, -1, -1, -1, -1, 0x68, -1, -1, -1, -1, 0x50 });
+struct ImageSectionHeader {
+    uint32_t bullshit1[4];
+    uint32_t size_of_segment;
+    uint32_t offset_to_segment;
+    uint32_t bullshit2[4];
+};
+
+void init_vulpes(){
+    // Get safe search bounds for CodeSignature.
+    ImageSectionHeader* header = reinterpret_cast<ImageSectionHeader*>(sig_text_segment_data.get_address());
+    set_lowest_permitted_address(0x400000 + header->offset_to_segment);
+    set_highest_permitted_address(0x400000 + header->offset_to_segment + header->size_of_segment);
+    // Check if we're a server.
+    if (sig_server.get_address()){
+        server = true;
+    }else{
+        RedirectIOToConsole();
+        server = false;
+    };
+    printf(_FOX);
+
+    init_hooks();
+}
+
+void destruct_vulpes(){
+    if (server){
+
+    }else{
+        FreeConsole();
+    };
+
+}
