6faab9d988478f107f977303f9787db4dc0aa740
gbMichelle <gbmichelle.dev@gmail.com>
2019-04-16 21:47:38 +0200

    object_unit got started on

---
+++ halo_functions/object_unit.cpp
@@ -0,0 +1,26 @@
+#include "object_unit.hpp"
+#include <cstdint>
+
+#include "../hooks/hooker.hpp"
+#include "../memory/object_unit.hpp"
+
+
+// Avoid calling these if you aren't sure about what you're doing.
+// A LOT of these functions don't have any checks to see if you gave them valid input.
+// Use the class functions in ObjectUnit instead.
+
+Signature(false, sig_unit_speak_func,
+    {0x53, 0x56, 0x8B, 0xF1, 0x8B, 0x0D, -1, -1, -1, -1, 0x8B, 0x49, 0x34});
+
+__attribute__((regparm(3)))
+void unit_speak(MemRef object, int16_t arg2, UnitSpeech* speech_data){
+    static uintptr_t func_unit_speak_ptr = sig_unit_speak_func.get_address();
+    if (func_unit_speak_ptr){
+        asm (
+            "push edi;\n"
+            "call %0;\n"
+            "pop edi;\n"
+            : "+m" (func_unit_speak_ptr)
+        );
+    };
+}
