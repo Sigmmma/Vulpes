2ab2b4c3741d30211413b3f86db29dec39f06b0a
gbMichelle <gbmichelle.dev@gmail.com>
2019-04-27 02:13:19 +0200

    Added the ability to detect what machine requested the command.

--- command/handler.cpp
+++ command/handler.cpp
@@ -1,6 +1,7 @@
 #include "handler.hpp"
 #include "../halo_functions/console.hpp"
 #include <vector>
+#include <cstdio>
 #include <string.h>
 #include <regex>
 
@@ -9,6 +10,7 @@ using namespace std;
 static vector<char*> commands;
 regex command_split("([^ \"]+)|\"([^\"]*)\"|\"([^\"]*)");
 
+// Adds Vulpes results to an already constructed autocomplete list.
 __attribute__((cdecl))
 void auto_complete(char* buffer[], uint16_t* current_index, const char* input){
     string input_str(input);
@@ -29,9 +31,11 @@ void auto_complete(char* buffer[], uint16_t* current_index, const char* input){
     *current_index = j;
 }
 
+// Returns false if the original Halo function should not fire after this.
 __attribute__((cdecl))
-bool process_command(char* input){
+bool process_command(char* input, int32_t network_machine_id){
     string input_str(input);
+    // A line starting with this should be ignored.
     if(input_str.find(";") == 0){
         return false;
     };
@@ -54,7 +58,7 @@ bool process_command(char* input){
                 if (semicolon_pos > 0){
                     matches.push_back(str.substr(0, semicolon_pos));
                 };
-                break;
+                break; // We shouldn't match anything after a semicolon
             };
             matches.push_back(str);
         }else if(arg_match[2] != ""){
