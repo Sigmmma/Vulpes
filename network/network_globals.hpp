493bbdb8b39d246915091ea477c000066e468fd8
gbMichelle <gbmichelle.dev@gmail.com>
2019-04-27 09:30:46 +0200

    Made network globals functional

--- network/network_globals.hpp
+++ network/network_globals.hpp
@@ -1,9 +1,10 @@
-Signature(true, sig_connection_type,
-    {0x66, 0x83, 0x3D, -1, -1, -1, -1, 0x02, 0x75, -1, 0x8B});
+#pragma once
+#include <cstdint>
+
 enum class ConnectionType : uint32_t {
     NONE = 0,
     CLIENT = 1,
     HOST = 2
 };
 
-ConnectionType* connection_type = reinterpret_cast<ConnectionType*>(sig_connection_type.get_address() + 3);
+ConnectionType get_connection_type();
