072ceb632439a61449b36c7929c5f9f3b8032263
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-01 07:49:31 +0200

    Command parsing and executing is a thing now. Todo:     Fix the exception when parsing strings that aren't actually numbers into ints.     Remove debug strings when that is done.     Make it log the finished commands.

--- command/handler.cpp
+++ command/handler.cpp
@@ -1,13 +1,25 @@
 #include "handler.hpp"
 #include "../halo_functions/console.hpp"
-#include <vector>
-#include <cstdio>
-#include <string.h>
 #include <regex>
+#include <cassert>
+#include <algorithm>
+#include <exception>
 
 using namespace std;
 
-static vector<char*> commands;
+class VulpesCommandException_WrongArgCount : public exception {
+    virtual const char* what() const throw(){
+        return "Input had an invalid argument count.";
+    }
+} wrong_arg_count_exception;
+
+class VulpesCommandException_UnparsableArg : public exception {
+    virtual const char* what() const throw(){
+        return "Input had an argument that couldn't be parsed.";
+    }
+} unparsable_arg_exception;
+
+static vector<VulpesCommand*> commands;
 regex command_split("([^[:space:]\"]+)|\"([^\"]*)\"|\"([^\"]*)");
 
 // Adds Vulpes results to an already constructed autocomplete list.
@@ -18,11 +30,11 @@ void auto_complete(char* buffer[], uint16_t* current_index, const char* input){
     int i = 0;
     int j = *current_index;
     while (i < commands.size() && *current_index < 256){
-        string cmp_str(commands[i]);
+        string cmp_str = commands[i]->get_name();
         if (input_str.size() <= cmp_str.size()){
             cmp_str.resize(input_str.size());
             if (input_str == cmp_str){
-                buffer[j] = commands[i];
+                buffer[j] = commands[i]->get_name().data();
                 j++;
             };
         };
@@ -68,9 +80,388 @@ bool process_command(char* input, int32_t network_machine_id){
         };
         search_start = arg_match.suffix().first;
     };
-    console_out_warning("Found args:");
-    for (int i=0; i < matches.size(); i++){
-        console_out(matches[i]);
+    printf("%s\n", "Got past split stage.");
+    string cmd_to_find = matches[0];
+    matches.erase(matches.begin());
+    transform(cmd_to_find.begin(), cmd_to_find.end(), cmd_to_find.begin(), ::tolower);
+
+    VulpesCommand* matching_cmd;
+    bool found_match = false; // this is used because checking if the pointer
+                              // for matching_cmd !=0 has weird behavior.
+    printf("Starting search for %s\n", cmd_to_find.data());
+    for (int i=0; i<commands.size(); i++){
+        if (commands[i]->get_name() == cmd_to_find){
+            matching_cmd = commands[i];
+            found_match = true;
+            printf("Broke out of search stage because of match %s\n", commands[i]->get_name().data());
+            break;
+        };
+    }; printf("%s\n", "Got past search stage.");
+    if (found_match){
+        printf("%s\n", "Found a match.");
+        vector<VulpesArg> parsed_args;
+        try {
+            parsed_args = matching_cmd->parse_args(matches);
+        }catch (exception& e){
+            console_out_error("Couldn't execute command.");
+            console_out_error(e.what());
+            return false;
+        };
+        matching_cmd->execute(parsed_args);
+        return false;
     };
+    printf("%s\n", "Got past everything.");
     return true;
+}
+
+VulpesArgDef::VulpesArgDef(string arg_name, bool is_optional, VulpesArgType arg_type){
+    type = arg_type;
+    name = arg_name;
+    optional = is_optional;
+    min_max = false;
+
+    if (arg_type==LONG){
+        imin = -2147483647;
+        imax = 2147483647;
+    } else if(arg_type==SHORT){
+        imin = -32767;
+        imax = 32767;
+    } else if(arg_type==CHAR){
+        imin = -127;
+        imax = 127;
+    } else if(arg_type==TIME){
+        imin = 0;
+        imax = 2147483647;
+    };
+
+    set_display_name();
+}
+
+VulpesArgDef::VulpesArgDef(string arg_name, bool is_optional, VulpesArgType arg_type, int max_char){
+    assert(arg_type==STRING);
+    type = arg_type;
+    name = arg_name;
+    optional = is_optional;
+    max_characters = max_char;
+    min_max = false;
+
+    set_display_name();
+}
+
+VulpesArgDef::VulpesArgDef(string arg_name, bool is_optional, VulpesArgType arg_type, int64_t min, int64_t max){
+    assert(arg_type==LONG || arg_type==SHORT || arg_type==CHAR || arg_type==TIME);
+    type = arg_type;
+    name = arg_name;
+    optional = is_optional;
+    min_max = true;
+
+    if (type==LONG){
+        assert(min >= -2147483647);
+        assert(max <= 2147483647);
+    } else if (type==SHORT){
+        assert(min >= -32767);
+        assert(max <= 32767);
+    } else if (type==CHAR){
+        assert(min >= -127);
+        assert(max <= 127);
+    };
+    imin = min;
+    imax = max;
+
+    set_display_name();
+}
+
+VulpesArgDef::VulpesArgDef(string arg_name, bool is_optional, VulpesArgType arg_type, float min, float max){
+    assert(arg_type==FLOAT);
+    type = arg_type;
+    name = arg_name;
+    optional = is_optional;
+    min_max = true;
+    fmin = min;
+    fmax = max;
+
+    set_display_name();
+}
+
+string VulpesArgDef::parse_str(string input){
+    if (max_characters){
+        return input.substr(0, max_characters);
+    };
+    return input;
+}
+
+int64_t parse_to_integer(string input){
+    int64_t output;
+    printf("parsing int\n");
+    if(input.find("0x") == 0){
+        output = stoi(input.substr(2, input.size()), 0, 16);
+    }else{
+        output = stoi(input);
+    };
+    printf("parsed int\n");
+    return output;
+}
+
+int32_t VulpesArgDef::parse_int(string input){
+    int32_t output;
+    int64_t parsed = parse_to_integer(input);
+    char error_str[80];
+    if(parsed > imax){
+        snprintf(error_str, 80,
+                "Input for arg %s too high. Highest allowed: %d, got %d.",
+                name.data(), imax, parsed);
+        console_out_error(error_str);
+        snprintf(error_str, 80,
+                "Setting it to %d.",
+                imax);
+        console_out_error(error_str);
+        output = imax;
+    }else if(parsed < imin){
+        snprintf(error_str, 80,
+                "Input for arg %s too low. Lowest allowed: %d, got %d.",
+                name.data(), imin, parsed);
+        console_out_error(error_str);
+        snprintf(error_str, 80,
+                "Setting it to %d.",
+                imax);
+        console_out_error(error_str);
+        output = imin;
+    }else{
+        output = static_cast<int32_t>(parsed);
+    };
+    return output;
+}
+
+float VulpesArgDef::parse_flt(string input){
+    float output = stof(input);
+    char error_str[80];
+    if(output > fmax){
+        snprintf(error_str, 80,
+                "Input for arg %s too high. Highest allowed: %f, got %f.",
+                name.data(), fmax, output);
+        console_out_error(error_str);
+        snprintf(error_str, 80,
+                "Setting it to %f.",
+                fmax);
+        console_out_error(error_str);
+        output = fmax;
+    }else if(output < fmin){
+        snprintf(error_str, 80,
+                "Input for arg %s too low. Lowest allowed: %f, got %f.",
+                name.data(), fmin, output);
+        console_out_error(error_str);
+        snprintf(error_str, 80,
+                "Setting it to %f.",
+                fmax);
+        console_out_error(error_str);
+        output = fmin;
+    };
+    return output;
+}
+
+bool VulpesArgDef::parse_bool(string input){
+    string parsed = input;
+    transform(parsed.begin(), parsed.end(), parsed.begin(), ::tolower);
+    if (input == "yes" || input == "on" || input == "true" || input == "1"){
+        return true;
+    }else if (input == "no" || input == "off" || input == "false" || input == "0"){
+        return false;
+    }else {
+        char error_str[80];
+        snprintf(error_str, 80,
+                "Couldn't parse input \"%s\" for %s. Assuming false.",
+                input.data(), name.data());
+        console_out(error_str);
+        return false;
+    };
+}
+
+void VulpesArgDef::set_display_name(){
+    if (optional){
+        name = name + "[";
+    }else{
+        name = name + "<";
+    };
+
+    switch (type){
+        case STRING :
+            name = name + "string";
+            goto end;
+        case LONG :
+            name = name + "long";
+            goto integer;
+        case SHORT :
+            name = name + "short";
+            goto integer;
+        case CHAR :
+            name = name + "byte";
+            goto integer;
+        case FLOAT :
+            name = name + "float";
+            goto floating_point;
+        case BOOL :
+            name = name + "on-off";
+            goto end;
+        case TIME :
+            name = name + "time";
+            goto end;
+    };
+
+    integer:
+    if (min_max){
+        name = name + "(" + to_string(imin) + "-" + to_string(imax) + ")";
+    };
+    goto end;
+
+    floating_point:
+    if (min_max){
+        name = name + "(" + to_string(fmin) + "-" + to_string(fmax) + ")";
+    };
+
+    end:
+    if (optional){
+        name = name + "]";
+    }else{
+        name = name + ">";
+    };
+}
+
+VulpesArg::VulpesArg(VulpesArgDef def, std::string in){
+    definition = &def;
+    input = in;
+
+    VulpesArgType type = definition->type;
+
+    if (type != STRING && input == ""){
+        output = false;
+        return;
+    };
+    try {
+        switch (type){
+            case STRING :
+                strout = definition->parse_str(input);
+                output = true;
+                break;
+            case LONG :
+            case SHORT :
+            case CHAR :
+                intout = definition->parse_int(input);
+                output = true;
+                break;
+            case FLOAT :
+                fltout = definition->parse_flt(input);
+                output = true;
+                break;
+            case BOOL :
+                boolout = definition->parse_bool(input);
+                output = true;
+                break;
+            case TIME :
+                //intout = parse_time(input);
+                output = false;
+                break;
+        };
+    }catch (exception& e){
+        throw unparsable_arg_exception;
+    };
+}
+
+int VulpesArg::int_out(){
+    return intout;
+}
+float VulpesArg::flt_out(){
+    return fltout;
+}
+bool VulpesArg::bool_out(){
+    return boolout;
+}
+string VulpesArg::str_out(){
+    return strout;
+}
+int VulpesArg::time_ticks(){
+    return intout;
+}
+int VulpesArg::time_seconds(){
+    return (intout/30);
+}
+bool VulpesArg::has_output(){
+    return output;
+}
+
+VulpesCommand::VulpesCommand(string cmd_name,
+                             bool (*function_to_exec)(vector<VulpesArg>),
+                             int num_args, ...){
+    name = cmd_name;
+    cmd_func = function_to_exec;
+    va_list va_args;
+    va_start(va_args, num_args);
+    for (int i=0; i<num_args; i++){
+        args.push_back(va_arg(va_args, VulpesArgDef));
+    };
+    va_end(va_args);
+    commands.push_back(this);
+    printf("Command %s added on index %d\n\n",
+           commands[commands.size()-1]->get_name().data(), commands.size()-1);
+}
+
+// Remove command from the command list on destruction.
+VulpesCommand::~VulpesCommand(){
+    for (int i=0;i<commands.size();i++){
+        if (commands[i] == this){
+            printf("Removing command %s by erasing index %d which contains: %s\n",
+                   name, i, commands[i]->get_name());
+            commands.erase(commands.begin() + i);
+            break;
+        };
+    };
+}
+
+string VulpesCommand::get_name(){
+    return name;
+}
+
+vector<VulpesArgDef> VulpesCommand::get_arg_defs(){
+    return args;
+}
+
+vector<VulpesArg> VulpesCommand::parse_args(vector<string> arg_strings){
+    char error_str[80];
+    int required_args;
+    for (required_args = 0; required_args<args.size(); required_args++){
+        if (args[required_args].optional){
+            break;
+        };
+    };
+    if (arg_strings.size() < required_args){
+        snprintf(error_str, 80,
+                "Too few args for command: %s. Got %d, expected %d to %d.",
+                name.data(), arg_strings.size(), required_args, args.size());
+        console_out(error_str);
+        throw wrong_arg_count_exception;
+    }else if (arg_strings.size() > args.size()){
+        snprintf(error_str, 80,
+                "Too many args for command: %s. Got %d, expected %d to %d.",
+                name.data(), arg_strings.size(), required_args, args.size());
+        console_out(error_str);
+        throw wrong_arg_count_exception;
+    };
+    vector<VulpesArg> parsed_args;
+    for (int i=0; i<arg_strings.size(); i++){
+        parsed_args.push_back(VulpesArg(args[i], arg_strings[i]));
+    };
+    return parsed_args;
+}
+
+bool VulpesCommand::execute(vector<VulpesArg> parsed_args){
+    bool success;
+    try {
+        success = cmd_func(parsed_args);
+    }catch(exception& e){
+        char error_str[80];
+        snprintf(error_str, 80,
+                "Execution of command %s failed. Exception: %s",
+                name.data(), e.what());
+        success = false;
+    };
+    return success;
 }
--- command/handler.hpp
+++ command/handler.hpp
@@ -1,9 +1,94 @@
 #pragma once
 #include <cstdint>
+#include <vector>
+#include <string>
+#include <stdarg.h>
+#include <math.h>
 
+// These functions are and should only be called by the game.
 __attribute__((cdecl))
 void auto_complete(char* buffer[], uint16_t* current_index, const char* input);
-
-
 __attribute__((cdecl))
 bool process_command(char* input, int32_t network_machine_id);
+
+// Interface for registering commands.
+
+enum VulpesArgType {
+    STRING,
+
+    LONG,
+    SHORT,
+    CHAR,
+
+    FLOAT,
+    BOOL,
+    TIME
+};
+
+class VulpesArgDef {
+public:
+    VulpesArgDef(std::string arg_name, bool is_optional, VulpesArgType arg_type);
+    VulpesArgDef(std::string arg_name, bool is_optional, VulpesArgType arg_type, int max_char);
+    VulpesArgDef(std::string arg_name, bool is_optional, VulpesArgType arg_type, int64_t min, int64_t max);
+    VulpesArgDef(std::string arg_name, bool is_optional, VulpesArgType arg_type, float min, float max);
+
+    std::string parse_str(std::string input);
+    int32_t parse_int(std::string input);
+    float parse_flt(std::string input);
+    bool parse_bool(std::string input);
+
+    std::string name;
+    std::string display_name;
+    VulpesArgType type;
+    bool min_max;
+    bool optional;
+    int64_t imax;
+    int64_t imin;
+    float fmin = -INFINITY;
+    float fmax = INFINITY;
+    int max_characters;
+private:
+    void set_display_name();
+};
+
+class VulpesArg {
+public:
+    VulpesArg(VulpesArgDef def, std::string in);
+
+    int         int_out();
+    float       flt_out();
+    bool       bool_out();
+    std::string str_out();
+    int      time_ticks();
+    int      time_seconds();
+
+    bool     has_output();
+
+private:
+    VulpesArgDef* definition;
+    std::string input;
+    bool output = false;
+
+    int intout;
+    float fltout;
+    bool boolout;
+    std::string strout;
+};
+
+class VulpesCommand {
+public:
+    VulpesCommand(std::string cmd_name,
+                  bool (*function_to_exec)(std::vector<VulpesArg>),
+                  int num_args, ...);
+    ~VulpesCommand();
+    std::string get_name();
+    std::vector<VulpesArgDef> get_arg_defs();
+    std::vector<VulpesArg> parse_args(std::vector<std::string> arg_strings);
+
+    bool execute(std::vector<VulpesArg> parsed_args);
+
+private:
+    std::string name;
+    std::vector<VulpesArgDef> args;
+    bool (*cmd_func)(std::vector<VulpesArg>);
+};
