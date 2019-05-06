31649ec1140b701d94a95d9dbbd0b04c8bd6e2f9
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-06 13:25:23 +0200

    Added the first debug command.

---
+++ command/debug.cpp
@@ -0,0 +1,23 @@
+#include "debug.hpp"
+#include "handler.hpp"
+#include "../halo_functions/console.hpp"
+#include "../halo_bug_fixes/shdr_trans_zfighting.hpp"
+
+bool toggle_shader_trans_fix(std::vector<VulpesArg> input){
+    bool on = input[0].bool_out();
+    if (on){
+        init_shdr_trans_zfighting_fixes();
+        console_out("Turned ON transparent fix.");
+    }else{
+        revert_shdr_trans_zfighting_fixes();
+        console_out("Turned OFF transparent fix.");
+    };
+    return true;
+}
+
+void init_debug_commands(){
+    static VulpesCommand cmd_dev_shader_transparent_fix(
+        "v_dev_shader_transparent_fix", &toggle_shader_trans_fix, 4, 1,
+        VulpesArgDef("", true, A_BOOL)
+    );
+}
