01ef865d2bfc53866290fddada853dc20a029a64
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-10 14:21:05 +0200

    Vulpes Messages can now be encoded by the mdp encoder without crashing, but the client still rejects them. (Debug prints will be left in until we can receive and confirm that the data is correct)

--- network/message_delta/vulpes_message.cpp
+++ network/message_delta/vulpes_message.cpp
@@ -1,16 +1,21 @@
 #include "bitstream.hpp"
 #include "unencoded_messages.hpp"
-#include "definition.hpp"
 #include "message_delta_processor.hpp"
 #include <cstring>
+#include <cstdio>
 
 __attribute__((cdecl))
 void handle_vulpes_message(MessageDeltaHeader* header){
     VulpesMessage decoded_message;
-
-    if (mdp_decode_stateless_iterated(header, (void*)&decoded_message)){
-        //process_vulpes_message(&decoded_message);
+    printf("Test packet was received!\n");
+    if (mdp_decode_stateless_iterated((void*)&decoded_message, header)){
+        printf("Got a length of: %d\nPayload contained:\n", decoded_message.payload_size);
+        for (int i = 0; i<decoded_message.payload_size; i++){
+            printf("%X ", decoded_message.payload8[i]);
+        };
+        printf("\n");
     };
+    mdp_discard_iteration_body(header);
 }
 
 __attribute__((cdecl))
@@ -19,17 +24,24 @@ void encode_vulpes_message(const MessageDeltaFieldPropertyDefinition* const defi
                            VulpesMessage* const input,
                            void* const    iteration_header){ // handles output buffer
 
+    printf("Vulpes message encoder was called!\n");
     bitstream_write(iteration_header, &input->payload_size, 16);
 
     bytes_left = input->payload_size;
-
+    printf("Payload size we got was: %d\n", input->payload_size);
+    printf("Payload we got was as:\n");
+    for (int i = 0; i<input->payload_size; i++){
+        printf("%X ", input->payload8[i]);
+    };
     for (int i=0; i<input->payload_size; i+=4) { // esi, edx, esp+4
         int32_t num_write_bytes = (input->payload_size - i) % 4;
         if (num_write_bytes == 0){
             num_write_bytes = 4;
         };
+        printf("Calling bitstream write.\n");
         bitstream_write(iteration_header, &input->payload32[i], num_write_bytes * 8);
     };
+    printf("Vulpes message encoder got done executing!\n");
 }
 
 __attribute__((cdecl))
@@ -38,6 +50,7 @@ void decode_vulpes_message(const MessageDeltaFieldPropertyDefinition* const defi
                            VulpesMessage* const output,
                            void* const    iteration_header){ // handles input buffer
 
+    printf("Vulpes message decoder was called!\n");
     bitstream_read(iteration_header, &output->payload_size, 16);
     // returns integer containing decoded bits
     for (int i=0; i<output->payload_size; i+=4) { // esi, edx, esp+4
