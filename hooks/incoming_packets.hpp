544ce9f9f98b8251aff2b5b089f5d336691b518a
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-11 09:50:32 +0200

    More hooks.

--- hooks/incoming_packets.hpp
+++ hooks/incoming_packets.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#include "../network/message_delta/unencoded_messages.hpp"
+#include "event.hpp"
+
+DEFINE_EVENT_HOOK(EVENT_RECEIVE_CHAT_MESSAGE, bool, HudChat*);
 
 void init_incoming_packet_hooks();
 void revert_incoming_packet_hooks();
