16df38dab8c493934566cf24ad5037b8450249ba
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-11 09:50:32 +0200

    More hooks.

--- hooks/map.hpp
+++ hooks/map.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include "event.hpp"
 
-DEFINE_EVENT_HOOK(EVENT_PRE_MAP_LOAD, void, char* map_name);
-DEFINE_EVENT_HOOK(EVENT_MAP_LOAD, void, char* map_name);
+DEFINE_EVENT_HOOK(EVENT_PRE_MAP_LOAD, void, char*);
+DEFINE_EVENT_HOOK(EVENT_MAP_LOAD, void, char*);
 
-DEFINE_EVENT_HOOK(EVENT_PRE_MAP_LOAD_MP, void, char* map_name);
-DEFINE_EVENT_HOOK(EVENT_MAP_LOAD_MP, void, char* map_name);
+DEFINE_EVENT_HOOK(EVENT_PRE_MAP_LOAD_MP, void, char*);
+DEFINE_EVENT_HOOK(EVENT_MAP_LOAD_MP, void, char*);
 
-DEFINE_EVENT_HOOK(EVENT_PRE_MAP_LOAD_SP_UI, void, char* map_name);
-DEFINE_EVENT_HOOK(EVENT_MAP_LOAD_SP_UI, void, char* map_name);
+DEFINE_EVENT_HOOK(EVENT_PRE_MAP_LOAD_SP_UI, void, char*);
+DEFINE_EVENT_HOOK(EVENT_MAP_LOAD_SP_UI, void, char*);
 
 void init_map_hooks();
 void revert_map_hooks();
