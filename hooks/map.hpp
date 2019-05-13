78eaa8d8561444786f7177aa0460c48612271fd4
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-13 08:21:38 +0200

    Fixed some oversights

--- hooks/map.hpp
+++ hooks/map.hpp
@@ -10,5 +10,5 @@ DEFINE_EVENT_HOOK(EVENT_MAP_LOAD_MP, void, char*);
 DEFINE_EVENT_HOOK(EVENT_PRE_MAP_LOAD_SP_UI, void, char*);
 DEFINE_EVENT_HOOK(EVENT_MAP_LOAD_SP_UI, void, char*);
 
-void init_map_hooks();
+void init_map_hooks(bool is_server);
 void revert_map_hooks();
