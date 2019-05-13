5ae6051d66a2eb08acf16fd68a31250bb9e5ad9f
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-13 03:22:09 +0200

    Made Object table not be initialization dependent. Added loads of tables.

--- memory/table.hpp
+++ memory/table.hpp
@@ -26,6 +26,14 @@ public:
     ObjectHeader* objects;
 };
 
-ObjectTable* get_object_table();
+ObjectTable* object_table();
 
-void init_tables();
+Table* weather_particles_table();
+Table* particle_table();
+Table* particle_system_particles_table();
+Table* effect_table();
+Table* effect_locations_table();
+Table* decals_table();
+Table* contrail_points_table();
+Table* contrail_table();
+Table* device_groups_table();
