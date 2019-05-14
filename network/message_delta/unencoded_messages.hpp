e7fdc3a794523443fdcf89569c894074f8490875
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-14 18:46:18 +0200

    added more packet structs

--- network/message_delta/unencoded_messages.hpp
+++ network/message_delta/unencoded_messages.hpp
@@ -236,6 +236,153 @@ struct PlayerScoreUpdate {
     float speed_multiplier;
 };
 
+struct GameEngineChangeMode {
+    int32_t mode; // Make an enum later.
+};
+
+struct GameEngineMapReset {
+    PAD(1);
+};
+
+struct MultiplayerHudMessage {
+    int32_t player_id;
+    int32_t message;
+    int32_t message_data;
+};
+
+struct MultiplayerSound {
+    int32_t id;
+};
+
+struct TeamChange {
+    int8_t player_id;
+    int8_t new_team_id;
+};
+
+struct UnitDropCurrentWeapon {
+    int32_t unit_id;
+    int32_t weapon_id;
+    bool immediate;
+};
+
+struct VehicleNew {
+    MemRef tag_id;
+    int32_t server_object_id;
+    int16_t owner_team_id;
+    PAD(2);
+    int16_t owner_player_id;
+    int32_t owner_object_id;
+    int32_t weapon_object_id[4];
+    Vec3d position;
+    Vec3d forward; // might want to normalize these
+    Vec3d up; // might want to normalize these
+    Vec3d translational_velocity;
+    Vec3d angular_velocity;
+    int8_t current_baseline_id;
+};
+
+struct BipedNew {
+    MemRef tag_id;
+    int32_t server_object_id;
+    int16_t owner_team_id;
+    PAD(2);
+    int16_t owner_player_id;
+    PAD(2);
+    int32_t owner_object_id;
+    Vec3d position;
+    Vec3d forward; // might want to normalize these
+    Vec3d up; // might want to normalize these
+    Vec3d translational_velocity;
+    RGBFloat change_color[4];
+    bool flashlight;
+    PAD(3);
+    float light_battery;
+    int8_t current_baseline_id;
+    PAD(3);
+    int16_t grenade_counts[2];
+    float health;
+    float shield;
+    bool shield_stun_ticks_greater_than_zero;
+};
+
+struct ProjectileNew {
+    MemRef tag_id;
+    int32_t server_object_id;
+    int16_t owner_team_id;
+    PAD(2);
+    int16_t owner_player_id;
+    PAD(2);
+    int32_t owner_object_id;
+    Vec3d position;
+    Vec3d forward; // might want to normalize these
+    Vec3d up; // might want to normalize these
+    Vec3d translational_velocity;
+    Vec3d angular_velocity;
+    int8_t current_baseline_id;
+};
+
+struct EquipmentNew {
+    MemRef tag_id;
+    int32_t server_object_id;
+    int16_t owner_team_id;
+    PAD(2);
+    int16_t owner_player_id;
+    PAD(2);
+    int32_t owner_object_id;
+    uint32_t object_flags;
+    Vec3d position;
+    Vec3d forward; // might want to normalize these
+    Vec3d up; // might want to normalize these
+    Vec3d translational_velocity;
+    Vec3d angular_velocity;
+    int8_t current_baseline_id;
+};
+
+struct WeaponNew {
+    MemRef tag_id;
+    int32_t server_object_id;
+    int16_t owner_team_id;
+    PAD(2);
+    int16_t owner_player_id;
+    PAD(2);
+    int32_t owner_object_id;
+    uint32_t object_flags;
+    Vec3d position;
+    Vec3d forward; // might want to normalize these
+    Vec3d up; // might want to normalize these
+    Vec3d translational_velocity;
+    int8_t current_baseline_id;
+    PAD(1);
+    int16_t magazine_totals[2];
+    float age;
+    int16_t magazine_loadeds[2];
+};
+
+struct GameSettingsUpdate {
+    char name[0x80];
+    char map_name[0x84];
+    char variant[0xD8];
+    int8_t unused_game_engine;
+    int8_t max_players;
+    int16_t difficulty;
+    int16_t player_count;
+    char players[0x200];
+    int32_t network_game_random_seed;
+    int32_t number_of_games_played;
+    int32_t local_data;
+};
+
+// struct ParametersControl
+
+struct LocalPlayerUpdate {
+    int8_t sequence_numbers;
+    int8_t last_completed_update_id;
+    PAD(2);
+    Vec3d position;
+};
+
+
+
 struct VulpesMessage {
     uint16_t payload_size;
     union {
