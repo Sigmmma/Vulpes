bd8fe5846775ccf77422038db4d5bd32623907c7
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-14 19:45:58 +0200

    Added a bunch of addresses and signature (safety commit)

---
+++ hooks/server.cpp
@@ -0,0 +1,2 @@
+intptr_t remove_quitting_players = 0x45BF00;
+intptr_t hill_move = 0x46CDB0;
---
+++ network/foxnet/projectile.cpp
@@ -0,0 +1,12 @@
+#define "../../hooks/hooker.hpp"
+
+static Signature(true, sig_bool_allow_client_side_projectiles,
+    {-1, -1, -1, -1, 0x01, 0x74, 0x15, 0x33, 0xC0, 0xEB, 0x16});
+
+static bool* allow_client_side_projectiles;
+
+void init_foxnet_projectile(){
+    allow_client_side_projectiles =
+        (bool*)*(uintptr_t*)sig_bool_allow_client_side_projectiles.get_address();
+
+}
