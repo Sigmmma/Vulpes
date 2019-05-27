55e8b66159e176aae85a1c568fbf21ef925379e6
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-27 06:44:42 +0200

    Rewrote the tweak that enables the console to use the new console_input_globals

--- halo/tweaks/tweaks.cpp
+++ halo/tweaks/tweaks.cpp
@@ -5,26 +5,14 @@
  */
 
 #include "tweaks.hpp"
-#include "../../hooker/hooker.hpp"
 #include "../hooks/tick.hpp"
-
-Signature(false, sig_console_enabled,
-    {0xA0, -1, -1, -1, -1, 0x84, 0xC0, 0x74, 0x28});
-
-bool* console_enabled;
-
-void enable_console(){
-    *console_enabled = true;
-    DEL_CALLBACK(EVENT_TICK, enable_console);
-}
+#include "../memory/gamestate/console.hpp"
 
 void init_tweaks(){
-    static intptr_t sig_addr1 = sig_console_enabled.address();
-    if (sig_addr1){
-        console_enabled = *reinterpret_cast<bool**>(sig_addr1+1);
-        ADD_CALLBACK(EVENT_TICK, enable_console);
-    };
+    console_input_globals()->enabled = true;
+    DEL_CALLBACK(EVENT_TICK, init_tweaks);
 }
 
 void revert_tweaks(){
+    DEL_CALLBACK(EVENT_TICK, init_tweaks);
 }
