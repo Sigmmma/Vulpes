f2462bebb6522906c2da2e6d52ca7729be17ddcb
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-29 07:26:47 +0200

    Debug command to disable client side projectiles

--- command/debug.cpp
+++ command/debug.cpp
@@ -8,6 +8,7 @@
 #include "handler.hpp"
 #include "../halo/functions/messaging.hpp"
 #include "../halo/fixes/shdr_trans_zfighting.hpp"
+#include "../halo/memory/gamestate/network.hpp"
 
 bool toggle_shader_trans_fix(std::vector<VulpesArg> input){
     bool on = input[0].bool_out();
@@ -21,9 +22,34 @@ bool toggle_shader_trans_fix(std::vector<VulpesArg> input){
     return true;
 }
 
+bool toggle_allow_client_side_projectiles(std::vector<VulpesArg> input){
+    if (input.size() > 0){
+        bool on = input[0].bool_out();
+        *allow_client_side_projectiles() = on;
+        if (on){
+            cprintf("Turned ON client side projectiles.");
+        }else{
+            cprintf("Turned OFF client side projectiles.");
+        };
+    }else{
+        if (*allow_client_side_projectiles()){
+            cprintf("Client side projectiles are ON.");
+        }else{
+            cprintf("Client side projectiles are OFF.");
+        };
+    };
+    return true;
+}
+
 void init_debug_commands(){
     static VulpesCommand cmd_dev_shader_transparent_fix(
-        "v_dev_shader_transparent_fix", &toggle_shader_trans_fix, 4, 1,
+        "v_dev_shader_transparent_fix",
+        &toggle_shader_trans_fix, 4, 1,
         VulpesArgDef("", true, A_BOOL)
+    );
+    static VulpesCommand cmd_dev_allow_client_side_projectiles(
+        "v_dev_allow_client_side_projectiles",
+        &toggle_allow_client_side_projectiles, 4, 1,
+        VulpesArgDef("", false, A_BOOL)
     );
 }
