e94bd5aef2d7262988c90524cc958079c697e96e
gbMichelle <gbmichelle.dev@gmail.com>
2019-05-31 00:42:52 +0200

    Added some oddball commands and documented part of the oddball globals. Closes #30

---
+++ halo/hooks/king.hpp
@@ -0,0 +1,10 @@
+/*
+ * Vulpes (c) 2019 gbMichelle
+ *
+ * This program is free software under the GNU General Public License v3.0 or later. See LICENSE for more information.
+ */
+
+#pragma once
+
+void init_king_hooks();
+void revert_king_hooks();
