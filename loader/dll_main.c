c83ee6fc9d1c088dedf660db489a26b69ddd1c44
gbMichelle <gbmichelle.dev@gmail.com>
2019-07-09 10:02:21 +0200

    Cleaned up a bit.

--- loader/dll_main.c
+++ loader/dll_main.c
@@ -1,10 +1,11 @@
 #define WIN32_MEAN_AND_LEAN
 #include <windows.h>
-#include <stdbool.h>
+
+// TODO: Add code that downloads and updates the mod.
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     if (fdwReason == DLL_PROCESS_ATTACH && GetModuleHandle("Vulpes.dll") == NULL) {
         LoadLibrary("Vulpes.dll");
     };
-    return true;
+    return TRUE;
 }
