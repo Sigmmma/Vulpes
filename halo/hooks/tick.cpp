71d3e92890d44cb20026f2d8d531777c4539237f
gbMichelle <gbmichelle.dev@gmail.com>
2019-08-13 23:16:42 +0200

    Moved tick hook to new hook method.

--- halo/hooks/tick.cpp
+++ halo/hooks/tick.cpp
@@ -10,12 +10,12 @@
 DEFINE_EVENT_HOOK_LIST(EVENT_PRE_TICK, pre_events);
 DEFINE_EVENT_HOOK_LIST(EVENT_TICK, events);
 
-bool before_tick(intptr_t original_args_ptr){
+extern "C" bool before_tick(intptr_t original_args_ptr){
     call_in_order(pre_events);
     return true;
 }
 
-void after_tick(intptr_t original_args_ptr){
+extern "C" void after_tick(intptr_t original_args_ptr){
     call_in_order(events);
 }
 
@@ -23,13 +23,25 @@ Signature(true, sig_tick,
     {0x51, 0x53, 0x68, 0xFF, 0xFF, 0x0F, 0x00, 0x68, 0x1F, 0x00, 0x09,
      0x00, 0xC6, 0x05, -1, -1, -1, -1, 0x01});
 
-Cave(tick_hook, sig_tick, 0, 7, &before_tick, &after_tick);
+extern "C" {
+
+    uintptr_t game_tick_actual_jmp;
+    extern game_tick_wrapper();
+
+}
+
+Patch(
+    tick_hook_patch,
+    sig_tick, 0, 7,
+    JMP_PATCH, &game_tick_wrapper
+);
 
 void init_tick_hook(){
-    tick_hook.build();
-    tick_hook.apply();
+    tick_hook_patch.build();
+    tick_hook_patch.apply();
+    game_tick_actual_jmp = tick_hook_patch.return_address();
 }
 
 void revert_tick_hook(){
-    tick_hook.revert();
+    tick_hook_patch.revert();
 }
