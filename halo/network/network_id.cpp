06b3f3d056d0d177f3d3f930db987ad331cd6471
gbMichelle <gbmichelle.dev@gmail.com>
2019-08-22 21:39:06 +0200

    Remove avoidable references to 'message_delta_index'

--- halo/network/network_id.cpp
+++ halo/network/network_id.cpp
@@ -7,8 +7,6 @@
 #include "../../hooker/hooker.hpp"
 #include "../memory/types.hpp"
 
-Signature(true, sig_message_delta_object_index, {0x8B, 0x44, 0x24, 0x14, 0x50, 0x8B, 0xCF, 0xB8, -1, -1, -1, -1, 0xE8});
-
 Signature(true, sig_func_server_register_network_index, {0x55, 0x56, 0x8B, 0x70, 0x58, 0x8A, 0x46, 0x0C, 0x8D, 0x6E, 0x0C, 0x57, 0x83, 0xCF, 0xFF, 0x3C, 0x01, 0x75});
 
 Signature(true, sig_func_client_register_network_index_from_remote, {0x8B, 0x40, 0x58, 0x8B, 0x50, 0x28, 0x53, 0x55, 0x8B, 0x6C, 0x24, 0x0C, 0x57, 0x8D, 0x3C, 0xAA, 0x8B, 0x17, 0x32, 0xDB, 0x83, 0xFA, 0xFF, 0x75});
@@ -38,6 +36,8 @@ struct SyncedObjectHeader{
     MemRef* translation_index;      //0x28 // same as network_translation_table
 };
 
+// Pointer to a pointer because the game might change the actual pointer
+// So we want to read what is in the game's pointer to make sure we're safe.
 SyncedObjectHeader** synced_objects_header = NULL;
 
 int32_t server_register_network_index(MemRef object){
@@ -48,13 +48,13 @@ int32_t server_register_network_index(MemRef object){
     };
     int32_t network_id;
     asm (
-        "mov eax, %[message_delta_object_index];"
+        "mov eax, %[synced_objects];"
         "push %[local_object_id];"
         "call %[server_register_network_index];"
         "mov %[network_id], eax;"
         "add esp, 4;"
         :
-        : [message_delta_object_index] "m" (message_delta_object_index),
+        : [synced_objects] "m" (synced_objects),
           [network_id] "m" (network_id),
           [local_object_id] "m" (object.raw),
           [server_register_network_index] "m" (func_server_register_network_index)
@@ -63,14 +63,15 @@ int32_t server_register_network_index(MemRef object){
 }
 
 void register_network_index_from_remote(int32_t network_id, MemRef object){
+    SyncedObjectHeader* synced_objects = *synced_objects_header;
     asm (
-        "mov eax, %[message_delta_object_index];"
+        "mov eax, %[synced_objects];"
         "mov ecx, %[local_object_id];"
         "push %[network_id];"
         "call %[client_register_network_index_from_remote];"
         "add esp, 4;"
         :
-        : [message_delta_object_index] "m" (message_delta_object_index),
+        : [synced_objects] "m" (synced_objects),
           [local_object_id] "m" (object.raw),
           [network_id] "m" (network_id),
           [client_register_network_index_from_remote] "m" (func_client_register_network_index_from_remote)
@@ -78,12 +79,26 @@ void register_network_index_from_remote(int32_t network_id, MemRef object){
 }
 
 void unregister_network_index(MemRef object){
+    /*
+     * We're creating this struct here because the original code looks for
+     * synced_objects at the offset 0x58 from the pointer in eax.
+     *
+     * What it does:
+     * mov edi, [eax+58h]
+     *
+     * This is just easier and more reliable than writing an ASM workaround.
+     */
+    struct {
+        PAD(0x58);
+        SyncedObjectHeader* synced_objects = *synced_objects_header;
+    }synced_objects;
+    auto synced_objects_ptr = &synced_objects;
     asm (
-        "mov eax, %[message_delta_object_index];"
+        "mov eax, %[synced_objects_ptr];"
         "mov esi, %[local_object_id];"
         "call %[unregister_network_index];"
         :
-        : [message_delta_object_index] "m" (message_delta_object_index),
+        : [synced_objects_ptr] "m" (synced_objects_ptr),
           [local_object_id] "m" (object.raw),
           [unregister_network_index] "m" (func_unregister_network_index)
     );
@@ -106,8 +121,10 @@ int32_t get_network_id_from_object(MemRef object){
 
 void init_network_id(){
     synced_objects_header = reinterpret_cast<SyncedObjectHeader**>(*reinterpret_cast<uintptr_t**>(sig_translation_table_ptr.address()));
-    message_delta_object_index = *reinterpret_cast<uintptr_t*>(sig_message_delta_object_index.address() + 8);
-    func_server_register_network_index = sig_func_server_register_network_index.address();
-    func_client_register_network_index_from_remote = sig_func_client_register_network_index_from_remote.address();
+    // +3 because if we enter the function on the second instruction
+    // we only need to supply the pointer to the synced_object_header.
+    func_server_register_network_index = sig_func_server_register_network_index.address() + 3;
+    func_client_register_network_index_from_remote = sig_func_client_register_network_index_from_remote.address() + 3;
+    // Not here because the function is a little more complicated.
     func_unregister_network_index = sig_func_unregister_network_index.address();
 }
