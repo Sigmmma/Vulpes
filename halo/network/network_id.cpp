8ec70f78ea4e43104317477ceabb68db275f9411
gbMichelle <gbmichelle.dev@gmail.com>
2019-08-22 04:02:34 +0200

    Fixed network_id

--- halo/network/network_id.cpp
+++ halo/network/network_id.cpp
@@ -7,8 +7,6 @@
 #include "../../hooker/hooker.hpp"
 #include "../memory/types.hpp"
 
-Signature(true, sig_message_delta_object_index, {0x8B, 0x44, 0x24, 0x14, 0x50, 0x8B, 0xCF, 0xB8, -1, -1, -1, -1, 0xE8});
-
 Signature(true, sig_func_server_register_network_index, {0x55, 0x56, 0x8B, 0x70, 0x58, 0x8A, 0x46, 0x0C, 0x8D, 0x6E, 0x0C, 0x57, 0x83, 0xCF, 0xFF, 0x3C, 0x01, 0x75});
 
 Signature(true, sig_func_client_register_network_index_from_remote, {0x8B, 0x40, 0x58, 0x8B, 0x50, 0x28, 0x53, 0x55, 0x8B, 0x6C, 0x24, 0x0C, 0x57, 0x8D, 0x3C, 0xAA, 0x8B, 0x17, 0x32, 0xDB, 0x83, 0xFA, 0xFF, 0x75});
@@ -17,7 +15,6 @@ Signature(true, sig_func_unregister_network_index, {0x53, 0x57, 0x8B, 0x78, 0x58
 
 Signature(true, sig_translation_table_ptr, {-1, -1, -1, -1, 0x8B, 0x52, 0x28, 0x8B, 0x34, 0x8A});
 
-uintptr_t message_delta_object_index = *reinterpret_cast<uintptr_t*>(sig_message_delta_object_index.address() + 8);
 uintptr_t func_server_register_network_index = sig_func_server_register_network_index.address();
 uintptr_t func_client_register_network_index_from_remote = sig_func_client_register_network_index_from_remote.address();
 uintptr_t func_unregister_network_index = sig_func_unregister_network_index.address();
@@ -38,7 +35,7 @@ struct SyncedObjectHeader{
 };
 
 int32_t server_register_network_index(MemRef object){
-    SyncedObjectHeader* synced_objects = reinterpret_cast<SyncedObjectHeader*>(message_delta_object_index + 0x58);
+    SyncedObjectHeader* synced_objects = reinterpret_cast<SyncedObjectHeader*>(**reinterpret_cast<uintptr_t**>(sig_translation_table_ptr.address()));
     if (synced_objects->count >= synced_objects->max_count){
         // Just don't even try if we're at max capacity.
         return -1;
@@ -85,15 +82,14 @@ void unregister_network_index(MemRef object){
 }
 
 MemRef get_object_from_network_index(int32_t network_id){
-    static MemRef* network_translation_table = reinterpret_cast<MemRef*>(*reinterpret_cast<uintptr_t*>(sig_translation_table_ptr.address()) + 0x28);
-    return network_translation_table[network_id];
+    SyncedObjectHeader* synced_objects = reinterpret_cast<SyncedObjectHeader*>(**reinterpret_cast<uintptr_t**>(sig_translation_table_ptr.address()));
+    return synced_objects->translation_index[network_id];
 }
 
 int32_t get_network_id_from_object(MemRef object){
-    SyncedObjectHeader* synced_objects = reinterpret_cast<SyncedObjectHeader*>(message_delta_object_index + 0x58);
-    static MemRef* network_translation_table = reinterpret_cast<MemRef*>(*reinterpret_cast<uintptr_t*>(sig_translation_table_ptr.address()) + 0x28);
+    SyncedObjectHeader* synced_objects = reinterpret_cast<SyncedObjectHeader*>(**reinterpret_cast<uintptr_t**>(sig_translation_table_ptr.address()));
     for (int i=1; i < synced_objects->max_count; i++){
-        if (network_translation_table[i].raw == object.raw){
+        if (synced_objects->translation_index[i].raw == object.raw){
             return i;
         };
     };
