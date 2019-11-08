fa31278cae5869e91d5cc9f932db43cbeafd95dd
gbMichelle <gbmichelle.dev@gmail.com>
2019-11-08 06:08:45 +0100

    [lua] Update console library rename cprint() -> console.print(). add console.clear()

--- vulpes/lua/lua_messaging.cpp
+++ vulpes/lua/lua_messaging.cpp
@@ -4,7 +4,11 @@
  * This program is free software under the GNU General Public License v3.0 or later. See LICENSE for more information.
  */
 
+#include <string>
+
 #include <lua.hpp>
+
+#include <util/nanoluadict.hpp>
 
 #include <vulpes/functions/messaging.hpp>
 
@@ -12,14 +16,32 @@
 #include "lua_messaging.hpp"
 
 static int luaV_cprint(lua_State *state) noexcept {
-    // Check if the number of arguments is correct for a print call.
-    if(lua_gettop(state) == 1) {
-        cprintf("%s", luaV_check_as_string(state, 1).data());
-        return 0;
+    int num_args = lua_gettop(state);
+
+    std::string combined = "";
+    // Arguments from the lua state start at index 1.
+    for (size_t i = 1; i <= num_args; i++){
+         combined += luaV_check_as_string(state, i) + " ";
     }
-    else return luaL_error(state,"wrong number of arguments used for cprint()");
+
+    cprintf("%s", combined.data());
+    return 0;
+}
+
+static int luaV_console_clear(lua_State *state){
+    if (lua_gettop(state)) {
+        luaL_error(state, "console.clear does not take any args.");
+    } else {
+        console_clear();
+    }
+    return 0;
 }
 
 void luaV_reg_messaging_funcs(lua_State *state) {
-    lua_register(state, "cprint", luaV_cprint);
+    luaDict(state,
+        "console",
+        2,
+        kvPairWithCFunction("print", luaV_cprint),
+        kvPairWithCFunction("clear", luaV_console_clear)
+    );
 }
