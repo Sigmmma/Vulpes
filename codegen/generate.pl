04fed8c67e20cb50040e8d088ec77f21172f8424
gbMichelle <gbmichelle.dev@gmail.com>
2019-12-13 23:52:15 +0100

    Add copyright headers

--- codegen/generate.pl
+++ codegen/generate.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+#
+# Vulpes (c) 2019 gbMichelle
+#
+# This program is free software under the GNU General Public License v3.0 or later. See LICENSE for more information.
+#
 
 use strict;
 use warnings;
