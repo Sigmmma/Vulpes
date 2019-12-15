ee04eb230921f0d5b98420bc88c1485abf65d1e4
gbMichelle <gbmichelle.dev@gmail.com>
2019-12-15 20:20:41 +0100

    [util] Added some spacing at the end of the cutie

--- util/fox.hpp
+++ util/fox.hpp
@@ -4,6 +4,5 @@ const char* _FOX =
 "_,-=._              /|_/|\n"
 "`-.}   `=._,.-=-._.,  @ @._,\n"
 "   `._ _,-.   )      _,.-'\n"
-"      `    G.m-\"^m`m'\n";
+"      `    G.m-\"^m`m'\n\n";
 //Cutie by Dmytro O. Redchuk
-
