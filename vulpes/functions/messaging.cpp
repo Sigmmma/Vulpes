06823c875ca1fd1cf21b2941a795c0bb8f27f9c8
gbMichelle <gbmichelle.dev@gmail.com>
2019-12-19 13:39:36 +0100

    [refactor] Add messaging signatures to signatures.yaml

--- vulpes/functions/messaging.cpp
+++ vulpes/functions/messaging.cpp
@@ -8,23 +8,17 @@
 #include <cstring>
 #include <string>
 
-#include <hooker/hooker.hpp>
-
 #include <vulpes/memory/gamestate/console.hpp>
+#include <vulpes/memory/signatures.hpp>
 
 #include "messaging.hpp"
-
-Signature(true, sig_console_line_new,
-    {0x8B, 0x15, -1, -1, -1, -1, 0x66, 0x83, 0x7A, 0x2E, 0x20, 0x75, 0x11});
-Signature(false, sig_console_to_terminal_and_network,
-    {0x55, 0x8B, 0xEC, 0x83, 0xE4, 0xF8, 0x81, 0xEC, 0x10, 0x01, 0x00, 0x00, 0x56});
 
 typedef uint32_t (*FpReturnsInt)();
 
 ConsoleOutput* console_new_line() {
     ConsoleGlobals* globals = console_globals();
     static auto console_line_new =
-        reinterpret_cast<FpReturnsInt>(sig_console_line_new.address());
+        reinterpret_cast<FpReturnsInt>(func_console_line_new());
     MemRef line;
     line.raw = console_line_new();
 
@@ -51,7 +45,7 @@ typedef __attribute__((regparm(1)))void (*ConsoleToTerminalAndNetworkCall)(void*
 void vcprintf(const ARGBFloat& color, const char* format, va_list args) {
     static auto console_to_terminal_and_network =
         reinterpret_cast<ConsoleToTerminalAndNetworkCall>(
-        sig_console_to_terminal_and_network.address());
+                func_console_to_terminal_and_network());
     ConsoleGlobals* globals = console_globals();
     if (globals->initialized) {
         ConsoleOutput* output = console_new_line();
@@ -59,7 +53,9 @@ void vcprintf(const ARGBFloat& color, const char* format, va_list args) {
             vsnprintf(&output->text[0], 255, format, args);
             output->color = color;
             output->tab_stops = strstr(output->text, "|t") != NULL;
-            console_to_terminal_and_network(&output->text);
+            if (console_to_terminal_and_network) {
+                console_to_terminal_and_network(&output->text);
+            }
         }
     }
 }
