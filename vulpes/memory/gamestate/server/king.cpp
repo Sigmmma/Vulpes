768b153cdad9d3d19442f415f47ef09736f79de3
gbMichelle <gbmichelle.dev@gmail.com>
2019-12-23 06:21:36 +0100

    [refactor] Even more and more signature porting and cleanup

--- vulpes/memory/gamestate/server/king.cpp
+++ vulpes/memory/gamestate/server/king.cpp
@@ -4,18 +4,12 @@
  * This program is free software under the GNU General Public License v3.0 or later. See LICENSE for more information.
  */
 
-#include <hooker/hooker.hpp>
+#include <vulpes/memory/signatures.hpp>
 
 #include "king.hpp"
 
-Signature(true, sig_king_globals,
-    {0xB9, 0x6B, 0x00, 0x00, 0x00, 0xBF, -1, -1, -1, -1,
-     0xF3, 0xAB, 0xB9, 0x6B, 0x00, 0x00, 0x00});
-
 KingGlobals* king_globals() {
-    static KingGlobals* king_globals_ptr =
-        *reinterpret_cast<KingGlobals**>(sig_king_globals.address()+6);
-    return king_globals_ptr;
+    return *reinterpret_cast<KingGlobals**>(sig_king_globals());
 }
 
 KingGlobalsUpgrade king_globals_upgrade_data;
