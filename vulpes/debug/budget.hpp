8f3060533a3e05da06cc6bf8a1647c4e8e280221
gbMichelle <gbmichelle.dev@gmail.com>
2020-03-14 08:48:08 +0100

    Don't forget pragma on budget's header

--- vulpes/debug/budget.hpp
+++ vulpes/debug/budget.hpp
@@ -4,5 +4,7 @@
  * This program is free software under the GNU General Public License v3.0 or later. See LICENSE for more information.
  */
 
+#pragma once
+
 // Print all tracked budgets to the user console.
 void cprint_budget();
