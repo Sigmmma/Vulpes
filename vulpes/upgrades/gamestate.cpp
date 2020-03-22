57797f3c8dea5b8a579fa2257905f49b9afc70d2
gbMichelle <gbmichelle.dev@gmail.com>
2020-03-22 21:11:43 +0100

    Condense code for new approach

--- vulpes/upgrades/gamestate.cpp
+++ vulpes/upgrades/gamestate.cpp
@@ -18,93 +18,17 @@
 
 #include "gamestate.hpp"
 
-static void table_set_up(GenericTable* table, const char* name, uint32_t element_size, uint32_t element_max) {
-    memset(table, 0, sizeof(GenericTable));
-    strncpy(table->name, name, sizeof(table->name) - 1);
-    table->max_elements = element_max;
-    table->element_size = element_size;
-    // Just hex for the NOT terminated string 'd@t@'
-    table->sig = 0x64407440; //'d@t@'
-    // Mark as invalid as that is what vanilla does.
-    table->is_valid = false;
-}
-
 // Vanilla
 
-static uint32_t* game_state_globals;
-static uint32_t* game_state_globals_cpu_allocation_size;
-uint32_t gamestate_new_return_address;
+static uintptr_t* game_state_globals;
+static uintptr_t* game_state_globals_cpu_allocation_size;
 
-// We're replacing the stock table allocation function for no reason other
-// than that I have been in stack debug hell for three hours now.
-// And I got shit to do.
-// So, this here does almost exactly what stock does.
-// Except for things I stripped for speed (tm)
-static GenericTable* gamestate_table_new_vanilla_memory(
-        const char* name, uint32_t element_size, uint32_t element_max) {
-    // Get the next available spot in vanilla gamestate.
-    auto new_table = reinterpret_cast<GenericTable*>(
-            *game_state_globals + *game_state_globals_cpu_allocation_size);
+// Upgrades
 
-    // Do generic table setup.
-    table_set_up(new_table, name, element_size, element_max);
-
-    // Calculate total used vanilla gamestate.
-    auto new_alloc_size = *game_state_globals_cpu_allocation_size
-                          + sizeof(GenericTable)
-                          + element_size * element_max;
-    // Store the total.
-    *game_state_globals_cpu_allocation_size = new_alloc_size;
-
-    // The first entry in the table in vanilla gamestate is right after the
-    // header.
-    new_table->first_int = reinterpret_cast<uintptr_t>(new_table) + sizeof(GenericTable);
-
-    printf("Vanilla gamestate allocation size %d/%d\n", new_alloc_size, 0x440000);
-
-    return new_table;
-}
-
-static size_t used_tables = 0;
-static size_t used_memory = 0;
+static uintptr_t used_extension_memory = 0;
 static const size_t ALLOCATED_UPGRADE_MEMORY = 5*1024*1024;
 static void* gamestate_extension_buffer;
 static void* gamestate_extension_checkpoint_buffer;
-
-static const size_t UPGRADE_TABLES = 32;
-static GenericTable* tables[UPGRADE_TABLES];
-static GenericTable tables_checkpoint[UPGRADE_TABLES];
-static uintptr_t table_offsets[UPGRADE_TABLES];
-
-static GenericTable* gamestate_table_new_upgrade_memory(
-        const char* name, uint32_t element_size, uint32_t element_max) {
-    // Pick the next available table.
-    size_t i = used_tables;
-    used_tables++;
-
-    // Never should we have more tables than the amount we allocated. Duh.
-    assert(i < UPGRADE_TABLES);
-
-    auto new_table = reinterpret_cast<GenericTable*>(
-            *game_state_globals + *game_state_globals_cpu_allocation_size);
-    tables[i] = new_table;
-    *game_state_globals_cpu_allocation_size = *game_state_globals_cpu_allocation_size + sizeof(GenericTable);
-    // Do generic table setup.
-    table_set_up(new_table, name, element_size, element_max);
-
-    // Make the first element be the next available spot in memory.
-    new_table->first_int = reinterpret_cast<size_t>(gamestate_extension_buffer) + used_memory;
-
-    table_offsets[i] = used_memory;
-
-    // Update the used memory.
-    used_memory += element_size * element_max;
-    assert(used_memory <= ALLOCATED_UPGRADE_MEMORY);
-
-    printf("Used upgrade memory: %d/%d\n", used_memory, ALLOCATED_UPGRADE_MEMORY);
-
-    return new_table;
-}
 
 struct TableUpgradeData {
     const char* name; // Name to apply this upgrade to.
@@ -172,16 +96,17 @@ struct CheckpointFileHeader {
 extern "C" __attribute__((regparm(1)))
 GenericTable* gamestate_table_new_replacement(uint32_t element_size,
         const char* name, uint16_t element_max) {
-    GenericTable* new_table;
     printf(
         "Table creation request:\n"
         "name:%-48s element_size:%8d element_max:%8d\n", name, element_size, element_max);
+    fflush(stdout);
 
     bool found = false;
     bool use_upgrade_memory = false;
     size_t i = 0;
     while (!found && TABLE_UPGRADES[i].new_max != 0) {
         if (strncmp(name, TABLE_UPGRADES[i].name, 31) == 0) {
+            // Set max to new max.
             element_max = TABLE_UPGRADES[i].new_max;
             use_upgrade_memory = TABLE_UPGRADES[i].in_upgrade_memory;
             printf("Upgrading element_max to %d\n", element_max);
@@ -191,11 +116,38 @@ GenericTable* gamestate_table_new_replacement(uint32_t element_size,
         i++;
     }
 
+    uintptr_t* mem_start;
+    uintptr_t* mem_used;
+
     if (use_upgrade_memory) {
-        new_table = gamestate_table_new_upgrade_memory(name, element_size, element_max);
+        mem_start = reinterpret_cast<uintptr_t*>(&gamestate_extension_buffer);
+        mem_used  = &used_extension_memory;
     } else {
-        new_table = gamestate_table_new_vanilla_memory(name, element_size, element_max);
+        mem_start = game_state_globals;
+        mem_used  = game_state_globals_cpu_allocation_size;
     }
+
+    // Get the next available spot in vanilla gamestate.
+    auto new_table = reinterpret_cast<GenericTable*>(*mem_start + *mem_used);
+
+    // Do the same setup that Halo does.
+    memset(new_table, 0, sizeof(GenericTable));
+    strncpy(new_table->name, name, sizeof(new_table->name) - 1);
+    new_table->max_elements = element_max;
+    new_table->element_size = element_size;
+    // Just hex for the NOT terminated string 'd@t@'
+    new_table->sig = 0x64407440; //'d@t@'
+    // Mark as invalid as that is what vanilla does.
+    new_table->is_valid = false;
+
+    // The first entry in the table in vanilla gamestate is right after the
+    // header.
+    new_table->first_int = reinterpret_cast<uintptr_t>(new_table) + sizeof(GenericTable);
+
+    // Calculate and store the total used.
+    *mem_used = *mem_used + sizeof(GenericTable) + element_size * element_max;
+
+    printf("Gamestate allocation size %d/%d\n", *mem_used, 0x440000);
 
     return new_table;
 }
@@ -204,28 +156,11 @@ static void save_checkpoint_upgrade() {
     printf("Saving upgraded gamestate...");
     memcpy(gamestate_extension_checkpoint_buffer, gamestate_extension_buffer, ALLOCATED_UPGRADE_MEMORY);
 
-    GenericTable tables_checkpoint[used_tables];
-    for (int i=0; i<used_tables; i++) {
-        memcpy(&tables_checkpoint[i], tables[i], sizeof(GenericTable));
-    }
-
     auto save_file_path = std::string(profile_path()) + "\\savegame.vulpes";
 
     FILE* save_file = fopen(save_file_path.c_str(), "wb");
 
-    // Prepare save_file header.
-    CheckpointFileHeader header;
-    memset(&header, 0, sizeof(header));
-    header.memory_layout_crc = 0;
-    header.tables_count = used_tables;
-    header.tables_offset = sizeof(header);
-    header.arrays_offset = header.tables_offset + sizeof(tables);
-    header.arrays_size = used_memory;
-
-    // Write all the relevant data to the file.
-    fwrite(&header, 1, sizeof(header), save_file);
-    fwrite(&tables_checkpoint, 1, sizeof(tables_checkpoint), save_file);
-    fwrite(gamestate_extension_checkpoint_buffer, 1, used_memory, save_file);
+    fwrite(gamestate_extension_checkpoint_buffer, 1, used_extension_memory, save_file);
 
     fflush(save_file);
     fclose(save_file);
@@ -235,28 +170,10 @@ static void save_checkpoint_upgrade() {
 static void load_checkpoint_upgrade() {
     printf("Loading upgraded gamestate...");
     auto save_file_path = std::string(profile_path()) + "\\savegame.vulpes";
+
     FILE* save_file = fopen(save_file_path.c_str(), "rb");
 
-    // Retrieve header
-    CheckpointFileHeader header;
-    fread(&header, 1, sizeof(header), save_file);
-
-    // These need to be the same.
-    assert(header.tables_count == used_tables);
-    assert(header.arrays_size == used_memory);
-
-    // Read table headers into checkpoint tables.
-    fseek(save_file, header.tables_offset, SEEK_SET);
-    fread(&tables_checkpoint, 1, sizeof(GenericTable) * used_tables, save_file);
-
-    // Make sure they're all the same types of tables.
-    for (int i=0; i < used_tables; i++) {
-        assert(strncmp(&tables[i]->name[0], &tables_checkpoint[i].name[0], 31) == 0);
-    }
-
-    // Last thing we read from the file is the saved checkpoint memory.
-    fseek(save_file, header.arrays_offset, SEEK_SET);
-    fread(&gamestate_extension_checkpoint_buffer, 1, used_memory, save_file);
+    fread(&gamestate_extension_checkpoint_buffer, 1, used_extension_memory, save_file);
 
     // Close the file.
     fclose(save_file);
@@ -278,11 +195,10 @@ void init_gamestate_upgrades() {
 
     auto patch_addr = sig_game_state_data_new();
 
-    game_state_globals = *reinterpret_cast<uint32_t**>(patch_addr+2);
-    game_state_globals_cpu_allocation_size = *reinterpret_cast<uint32_t**>(patch_addr+0x37);
+    game_state_globals = *reinterpret_cast<uintptr_t**>(patch_addr+2);
+    game_state_globals_cpu_allocation_size = *reinterpret_cast<uintptr_t**>(patch_addr+0x37);
 
     patch_gamestate_new_replacement.build(patch_addr);
-    gamestate_new_return_address = patch_gamestate_new_replacement.return_address();
     patch_gamestate_new_replacement.apply();
 
     // Allocate and null the memory for our upgrades.
@@ -295,7 +211,6 @@ void init_gamestate_upgrades() {
     assert(gamestate_extension_checkpoint_buffer);
 
     memset(gamestate_extension_buffer, 0, ALLOCATED_UPGRADE_MEMORY);
-
 
     ADD_CALLBACK_P(EVENT_BEFORE_SAVE, save_checkpoint_upgrade, EVENT_PRIORITY_FINAL);
     ADD_CALLBACK_P(EVENT_BEFORE_LOAD, load_checkpoint_upgrade, EVENT_PRIORITY_FINAL);
